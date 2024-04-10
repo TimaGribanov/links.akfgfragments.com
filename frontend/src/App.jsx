42b81c205e37944fa047e5c82a376ca519dbbbc7
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2024-04-10 10:09:58 +0200

    adding twitter and OG meta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Helmet from 'react-helmet'
 
 import './App.css'
 
@@ -32,6 +33,23 @@ const App = () => {
 
   return (
     <div className='container text-center w-75'>
+      <Helmet>
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={links.title} />
+        <meta property='og:url' content={window.location} />
+        <meta property='og:description' content='akfgfragments.com' />
+        <meta property='og:image' content={links.img_uri} />
+        <meta property='og:image:alt' content={links.title} />
+        <meta property='og:site_name' content='akfgfragments' />
+        
+        <meta property='twitter:card' content='website' />
+        <meta property='twitter:site' content='@AkfgfragmentsEn' />
+        <meta property='twitter:creator' content='@AkfgfragmentsEn' />
+        <meta property='twitter:description' content='akfgfragments.com' />
+        <meta property='twitter:title' content={links.title} />
+        <meta property='twitter:image' content={links.img_uri} />
+        <meta property='twitter:image:alt' content={links.title} />
+      </Helmet>
       <header><span>akfgfragments</span></header>
       <main className='mt-5'>
         <Title title={links.title} />
